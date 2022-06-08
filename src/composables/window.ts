1ef3c1a19b2d58b961fbf72bf94e31c18a61c7ad
sakamoto shinji <shinj0712.dev@gmail.com>
2022-06-08 22:47:36 +0900

    window.ts作成

---
+++ src/composables/window.ts
@@ -0,0 +1,44 @@
+import { Ref } from "vue";
+
+interface Breakpoints {
+  sm: number;
+  md: number;
+  lg: number;
+  wd: number;
+}
+
+export const useWindow = () => {
+  // ブレークポイントの定義
+  const breakpoints: Breakpoints = {
+    sm: 320,
+    md: 560,
+    lg: 1025,
+    wd: 1920,
+  };
+
+  // 表示幅によってデバイスの名前を返す関数です
+  const getDeviceName = (width: number): string => {
+    return (width < breakpoints.md) ? 'sp' : (width >= breakpoints.lg) ? 'pc' : (width >= breakpoints.md) ? 'tablet' : '';
+  }
+
+  // stateの定義
+  const windowWidth: Ref<number> = useState('windowWidth', () => window.innerWidth);
+  const windowHeight: Ref<number> = useState('windowHeight', () => window.innerHeight);
+  const responsiveDevice: Ref<string> = useState('responsiveDevice', () => getDeviceName(window.innerWidth));
+
+  const update = (event: any) => {
+    windowWidth.value = event.currentTarget.innerWidth;
+    windowHeight.value = event.currentTarget.innerHeight;
+    responsiveDevice.value = getDeviceName(windowWidth.value);
+  };
+
+  onMounted(() => window.addEventListener('resize', update));
+  onUnmounted(() => window.addEventListener('resize', update));
+
+  return {
+    windowWidth,
+    windowHeight,
+    breakpoints,
+    responsiveDevice,
+  };
+};
