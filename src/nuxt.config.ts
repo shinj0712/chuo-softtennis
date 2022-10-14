bafccf71eb6d9315aa959fe682ae460d0d9c91d1
sakamoto shinji <shinj0712.dev@gmail.com>
2022-10-14 22:42:32 +0900

    ssr設定を変更

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -3,7 +3,6 @@ import svgLoader from 'vite-svg-loader'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	ssr: false,
 	target: 'static',
 	typescript: { strict: true },
 
